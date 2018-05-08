d8a21124b2f919bf897cbfe3243f6106fe875be7
Diwakar Mishra <diwakarmishra.himt@gmail.com>
2018-05-08 09:40:24 +0800

    created by componenet modified

--- app/components/CreatedByView.js
+++ app/components/CreatedByView.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class CreatedByView extends Component {
+export default class CreatedByView extends Component {
 
     render() {
         return (
             <View>
-                <Text style={{ marginTop: 10, color: '#CCCCCC', alignContent: 'center' }}>This is a sample application to learn react native basic layout strategies and components. This page is made with help of FLEX and also using inline/import style.</Text>
-                <Text style={{ color: '#8C8C8C', textAlign: 'center' }}>Created By: Diwakar Mishra</Text>
+                {/* <Text style={{ marginTop: 10, color: '#CCCCCC', alignContent: 'center' }}>This is a sample application to learn react native basic layout strategies and components. This page is made with help of FLEX and also using inline/import style.</Text> */}
+                <Text style={{ color: '#8C8C8C', textAlign: 'center' }}>Author- Diwakar Mishra</Text>
+                <Text style={{ color: '#8C8C8C', textAlign: 'center' }}>https://github.com/Diwakar1988</Text>
             </View >
         );
     }
 }
-
-export default CreatedByView;
